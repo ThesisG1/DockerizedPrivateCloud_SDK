fcf7db22a80d8e38cfefae54b42a9b65e4b8ff50
Mostafa <mostafamibrahim12@gmail.com>
2024-03-26 12:01:30 +0200

    deleted unnecessary items

--- ros_level/ros_node.py
+++ ros_level/ros_node.py
@@ -37,29 +37,3 @@ class Node:
             writer.write_publishers(ros_node_info.node_publishers)
             writer.write_subscribers(ros_node_info.node_subscribers)
         parse = parser.Parser.parse_types(ros_node_info)
-
-    # # Parse the types found in the ['type'] field of the node_publishers and node_subscribers
-    # def parse_types(self, ros_node_info):
-    #     imports = set()
-    #     types = {}
-    #     for i in range(len(ros_node_info.node_publishers)):
-    #         type = ros_node_info.node_publishers[f"topic_{i+1}"]["type"]
-    #         if "/" in type:
-    #             import_type, type_name = type.split("/")
-    #             imports.add(import_type)
-    #             if import_type in types:
-    #                 types[import_type].append(type_name)
-    #             else:
-    #                 types[import_type] = [type_name]
-
-    #     for i in range(len(ros_node_info.node_subscribers)):
-    #         type = ros_node_info.node_subscribers[f"topic_{i+1}"]["type"]
-
-    #         if "/" in type:
-    #             import_type, type_name = type.split("/")
-    #             imports.add(import_type)
-    #             if type in types:
-    #                 types[import_type].append(type_name)
-    #             else:
-    #                 types[import_type] = [type_name]
-    #     return imports, types
